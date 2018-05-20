7010100455ca49c87004d8b6803562c52104cbff
David Klionsky <davidklionsky@gmail.com>
2018-05-20 11:46:24 -0700

    Clean up HTML and function names

--- postcards/postcards.js
+++ postcards/postcards.js
@@ -1,7 +1,7 @@
 // Hashes a string into an int
 // Reference implementation:
 // https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery
-function easy_hash(input) {
+function easyHash(input) {
   var hash = 0, i, chr;
   if (input.length === 0) return hash;
   for (i = 0; i < input.length; i++) {
@@ -12,30 +12,26 @@ function easy_hash(input) {
   return hash;
 }
 
-// checks user input string against list of valid answers
-// and returns true if answer matches, false otherwise
-// this function will canonicalize the input into a standard form
-// user_input: string
-// valid_answer_hashes: array<int>
-function check_answer(user_input, valid_answer_hashes)
-{
-    var inputStr = user_input.toLowerCase().replace(/[^a-z]/gi, '');
-    var inputHash = easy_hash(inputStr);
-    console.log("Hello " + inputStr + " hash: " + inputHash);
-    return valid_answer_hashes.includes(inputHash);
-}
+function checkAnswer(answerHashes) {
+    var userInput =  document.getElementById("answer").value;
+    var inputStr = userInput.toLowerCase().replace(/[^a-z]/gi, '');
+    var inputHash = easyHash(inputStr);
+    var isCorrect = answerHashes.includes(inputHash);
 
-function myFunction(valid_answer_hashes) {
-    var answerElem =  document.getElementById("answer");
-    var isCorrect = check_answer(answerElem.value, valid_answer_hashes);
     var feedbackStr = "";
-    if (isCorrect)
-    {
+    if (isCorrect) {
         feedbackStr = "That is correct!";
-    }
-    else
-    {
+    } else {
         feedbackStr = "Try again";
     }
-    document.getElementById("feedback_answer").innerHTML = feedbackStr;
+    document.getElementById("feedback").innerHTML = feedbackStr;
+}
+
+var hintIndex = 0;
+
+function giveHint(hints) {
+    document.getElementById("feedback").innerHTML += "<br>" + hints[hintIndex++];
+    if (hintIndex >= hints.length) {
+        document.getElementById("hint").disabled = true;
+    }
 }
