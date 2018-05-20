a7f2c185a9a2d36a6660f20e15f8e758babd63e8
David Klionsky <davidklionsky@gmail.com>
2018-05-20 12:43:46 -0700

    Add all solutions and images

--- postcards/postcards.js
+++ postcards/postcards.js
@@ -1,16 +1,21 @@
-// Hashes a string into an int
-// Reference implementation:
-// https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery
+// Yes, we know you could probably reverse engineer this (we're looking at you, Robert). 
+// If you figure out how to do that, let us know ;)
+
 function easyHash(input) {
-  var hash = 0, i, chr;
-  if (input.length === 0) return hash;
-  for (i = 0; i < input.length; i++) {
-    chr   = input.charCodeAt(i);
-    hash  = ((hash << 5) - hash) + chr;
-    hash |= 0; // Convert to 32bit integer
-  }
-  return hash;
+    // https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript-jquery
+    var hash = 0, i, chr;
+    if (input.length === 0) return hash;
+    for (i = 0; i < input.length; i++) {
+        chr   = input.charCodeAt(i);
+        hash  = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
 }
+
+var hintIndex = 0;
+var wrongAnswerIndex = 0;
+let wrongAnswerFeedbacks = ["That's not it", "That's not it, keep trying!", "That's not it, keep trying! We believe in you!", "That's not it, keep trying! We really believe in you!"];
 
 function checkAnswer(answerHashes) {
     var userInput =  document.getElementById("answer").value;
@@ -21,16 +26,16 @@ function checkAnswer(answerHashes) {
     var feedbackStr = "";
     if (isCorrect) {
         feedbackStr = "Correct!<br><br>P.S. Let us know how this one went ❤️";
+        document.getElementById("hints").innerHTML = "";
     } else {
-        feedbackStr = "Try again";
+        feedbackStr = wrongAnswerFeedbacks[wrongAnswerIndex];
+        wrongAnswerIndex = (wrongAnswerIndex + 1) % wrongAnswerFeedbacks.length;
     }
     document.getElementById("feedback").innerHTML = feedbackStr;
 }
 
-var hintIndex = 0;
-
 function giveHint(hints) {
-    document.getElementById("feedback").innerHTML += hints[hintIndex++] + "<br>";
+    document.getElementById("hints").innerHTML += hints[hintIndex++] + "<br>";
     if (hintIndex >= hints.length) {
         document.getElementById("hint").disabled = true;
     }
